3b7a0b1ee357b72143e9b23b3524ab278ba2cd74
Alexey Mezenin <alexeymezenin@gmail.com>
2023-01-03 15:35:17 +1030

    Modified User model json format

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   require 'securerandom'
 
   has_secure_password
+
+  def as_json(options={})
+    super(root: 'user', only: [:email, :username, :bio, :image])
+  end
 end
