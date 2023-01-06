11294751eaabe6839134c9f57ae1b3f9607cbbc0
Alexey Mezenin <alexeymezenin@gmail.com>
2023-01-06 16:30:25 +1030

    Test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,34 +5,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
-  test "should get index" do
-    get users_url, as: :json
-    assert_response :success
-  end
-
   test "should create user" do
     assert_difference("User.count") do
-      post users_url, params: { user: { bio: @user.bio, email: @user.email, image: @user.image, password: @user.password, username: @user.username } }, as: :json
+      post '/api/users', params: { user: { bio: @user.bio, email: 'different@email.com', image: @user.image, password: 'secret', username: 'Different username' } }, as: :json
     end
 
     assert_response :created
-  end
-
-  test "should show user" do
-    get user_url(@user), as: :json
-    assert_response :success
-  end
-
-  test "should update user" do
-    patch user_url(@user), params: { user: { bio: @user.bio, email: @user.email, image: @user.image, password: @user.password, username: @user.username } }, as: :json
-    assert_response :success
-  end
-
-  test "should destroy user" do
-    assert_difference("User.count", -1) do
-      delete user_url(@user), as: :json
-    end
-
-    assert_response :no_content
   end
 end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,4 @@
 require "test_helper"
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
